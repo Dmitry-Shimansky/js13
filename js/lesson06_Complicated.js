bc7c8322548a7bb85bb41d70ec451407350216ab
Dimitrius <shimansky.dmitry@gmail.com>
2024-11-03 16:41:45 +0200

    add: lesson 5 complicated

---
+++ js/lesson06_Complicated.js
@@ -0,0 +1,6 @@
+const arr = ['345523466', '23345543095', '94822098532', '883938820856', '74789293884', '998473721093', '454409578675'];
+
+const newArray = arr.filter((element) => element.charAt(0) == 4 || element.charAt(0) == 2);
+
+console.log(newArray);
+
